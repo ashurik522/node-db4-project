ce66026c1d75919c48e38184f4db048a7ac21f12
ashurik522 <ashurik522@gmail.com>
2022-07-21 10:25:40 -0400

    recipes router

---
+++ api/recipes/recipes-router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const {} = require('./recipes-middleware')
+const Recipes = require('./recipes-model')
+
+const router = express.Router()
+
+module.exports = router;
